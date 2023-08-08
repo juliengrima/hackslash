cba8c784110195befe0ce11e867d538d71499377
juliengrima <gogrinformatique@gmail.com>
2023-08-08 16:28:18 +0200

    Playermouvements / Cameramouvement

--- Assets/Prefabs/Player/Scrips/PlayerTag.cs
+++ Assets/Prefabs/Player/Scrips/PlayerTag.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerTag : MonoBehaviour
 {
     #region Champs
+    //RaycastHit2D hit;
+    //Vector2 direction;
     #endregion
     #region Unity LifeCycle
     // Start is called before the first frame update
@@ -20,7 +22,18 @@ public class PlayerTag : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //hit = Physics2D.Raycast(this.gameObject.transform.position, direction);
+        //Debug.DrawRay(transform.position, transform.rotation, Color.red);
+
+        ////If something was hit
+        //if (hit.collider != null)
+        //{
+        //    //If the object hit is less than or equal to 6 units away from this object.
+        //    if (hit.distance <= 6.0f)
+        //    {
+        //        Debug.Log("Enemy In Range!");
+        //    }
+        //}
     }
     #endregion
     #region Methods
