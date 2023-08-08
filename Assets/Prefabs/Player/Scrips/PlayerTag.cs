a23ae3a1193473d1b8b6806aec215bab841535ba
juliengrima <gogrinformatique@gmail.com>
2023-08-08 14:16:33 +0200

    Import Player

---
+++ Assets/Prefabs/Player/Scrips/PlayerTag.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTag : MonoBehaviour
+{
+    #region Champs
+    #endregion
+    #region Unity LifeCycle
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+    #region Methods
+    void FixedUpdate (){
+        
+    }
+    void LateUpdate (){
+        
+    }
+    #endregion
+}
