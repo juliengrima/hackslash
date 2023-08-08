d7c3cf9b081eb2a408f139d37974d30c02cb4e56
Swaon <swaon@hotmail.com>
2023-08-08 21:46:33 +0200

    ok c'est bon la ! J'suis veni ici pour souffrir ok !

--- git-filter.sh
+++ git-filter.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#vscode in Packages/manifest.json
-#sed '/^\s*"com\.unity\.ide\.vscode": "https:\/\/github\.com\/SwaonD\/VSCodePackage\.git",\s*$/d'
+file="$1"
+# use file to sed one time (solve pb that only the first sed were executed)
 
-#vscode in Packages/packages-lock.json
-sed '/"com\.unity\.ide\.vscode": {/,/},/d'
-
-sed '/^bonjour$/d'
-
+if [[ "$file" == "Packages/manifest.json" ]]; then
+	sed '/^\s*"com\.unity\.ide\.vscode": "https:\/\/github\.com\/SwaonD\/VSCodePackage\.git",\s*$/d'
+	# vscode in Packages/manifest.json
+elif [[ "$file" == "Packages/packages-lock.json" ]]; then
+	sed '/^\s*"com\.unity\.ide\.vscode": {/,/^\s*},\s*$/d'
+	# vscode in Packages/packages-lock.json
+fi
