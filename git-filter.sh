7739dedf6e5d58f2feb1199aaa9decd46f15a002
Swaon <swaon@hotmail.com>
2023-08-08 21:23:03 +0200

    new filter test

--- git-filter.sh
+++ git-filter.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #vscode in Packages/manifest.json
-sed '/^\s*"com\.unity\.ide\.vscode": "https:\/\/github\.com\/SwaonD\/VSCodePackage\.git",\s*$/d'
+#sed '/^\s*"com\.unity\.ide\.vscode": "https:\/\/github\.com\/SwaonD\/VSCodePackage\.git",\s*$/d'
 
 #vscode in Packages/packages-lock.json
 sed '/"com\.unity\.ide\.vscode": {/,/},/d'
+
+sed '/^bonjour$/d'
+
