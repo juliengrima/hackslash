7ea726ecea58c65f2c8bb3cc633a9e47db52cf62
Swaon <swaon@hotmail.com>
2023-08-08 20:14:31 +0200

    edit filter

--- git-filter.sh
+++ git-filter.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sed '/^\s*"com\.unity\.ide\.vscode": "https:\/\/github\.com\/SwaonD\/VSCodePackage\.git",\s*$/d'
+
+file_path="$1"
+
+sed -i '/^\s*"com\.unity\.ide\.vscode": "https:\/\/github\.com\/SwaonD\/VSCodePackage\.git",\s*$/d' "$file_path"
+
+exec cat "$file_path"
